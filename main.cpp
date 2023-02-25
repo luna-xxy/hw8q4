31ee97252a7133236e59899dead2157a4b13f003
Luna <121996747+luna-xxy@users.noreply.github.com>
2023-02-25 01:23:15 +0000

    Initial commit

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 #include <ctime>
 using namespace std;
 
-const int PIN[] = {1, 2, 3, 4, 5};
+const int PIN[] = {1, 2, 3, 4, 7};
 const int NUM_DIGITS = 10;
 const int LEN_PIN = 5;
 
 void printArray(int arr[], int arrSize);
-void generateRandDigits(int arr[], int length);
+void generateRandDigits(int numRand[NUM_DIGITS]);
 int generateRandPin(int numRand[]);
 
 int main() {
@@ -19,9 +19,8 @@ int main() {
   int numInPIN[NUM_DIGITS] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
   int numRand[NUM_DIGITS];
-  for (int i = 0; i < NUM_DIGITS; i++)
-      numRand[i] = 1 + (rand() % 3);
-
+  
+  generateRandDigits(numRand);
 
   cout << "PIN: ";
   printArray(numInPIN, NUM_DIGITS);
@@ -45,10 +44,10 @@ void printArray(int arr[], int arrSize){
   cout << endl;
 }
 
-// void generateRandDigits(int arr[], int length){
-//     for (int i = 0; i < length; i++)
-//       arr[i] = 1 + (rand() % 3);
-// }
+void generateRandDigits(int numRand[NUM_DIGITS]){
+    for (int i = 0; i < NUM_DIGITS; i++)
+      numRand[i] = 1 + (rand() % 3);
+}
 
 // couple of feedback:
 // 1. naming: arr1 and arr2 are not meaningful enough for this function, and made it hard to understand your code. Just use numInPIN and numRand. 
